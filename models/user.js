548a5909839994edf316cef02e8669db333cf01e
Luke Barnetson <117444399+lukebarnetson1@users.noreply.github.com>
2025-01-26 17:47:32 +0000

    feat: add user model file

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const User = sequelize.define(
+  "User",
+  {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: "Must be a valid email address",
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING, // Hashed password
+      allowNull: false,
+    },
+    isVerified: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    timestamps: true, // Adds createdAt and updatedAt fields
+  }
+);
+
+module.exports = User;
