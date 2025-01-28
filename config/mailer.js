394f2ba561680ba0669115c69e8a2f29f1de5df2
Luke Barnetson <117444399+lukebarnetson1@users.noreply.github.com>
2025-01-28 20:56:22 +0000

    feat: create config for sending emails

---
+++ config/mailer.js
@@ -0,0 +1,14 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: parseInt(process.env.EMAIL_PORT, 10),
+  secure: process.env.EMAIL_SECURE === "true", // true for 465, false for other ports
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+module.exports = transporter;
